f61248b8b1dcc15e9579c90ff1ea12a9c59edbce
marcella-merholz <marcella_merholz@web.de>
2021-06-12 09:25:01 +0200

    clear

--- model/user-repository.js
+++ model/user-repository.js
@@ -3,10 +3,23 @@ const sqlite = require('sqlite');
 
 module.exports = class UserRepository {
 
+  async openDb() {
+    return sqlite.open({
+      filename: 'users.db',
+      driver: sqlite3.Database
+    })
+  }
+
+  async init() {
+    const db = await this.openDb();
+    await db.exec('DROP TABLE IF EXISTS users');
+    await db.exec('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username Text NOT NULL, email Text NOT NULL, password Text NOT NULL);');
+    await db.exec('INSERT INTO users (username, email, password) VALUES ("Sabrina", "vonsab@gmx.at", "geheim47"),("Marcella", "marcella_merholz@web.de", "password7");');
+  }
+
   async addUser(userData) {
     try {
       const db = await this.openDb();
-      const { username, email, password } = userData;
       // const response = await db.exec(`INSERT INTO users (username, email, password) VALUES ("${username}", "${email}", "${password}");`);
       console.log(username, email, password)
       await db.run('INSERT INTO users (username, email, password) VALUES (?,?,?)', [userData.username, userData.email, userData.password,]);
@@ -25,25 +38,10 @@ module.exports = class UserRepository {
     return users;
   }
 
-
   async getUserByEmail(newEmail) {
     const db = await this.openDb();
     const user = await db.all('SELECT * FROM users WHERE email = ?', [newEmail]);
     return user;
-  }
-
-  async init() {
-    const db = await this.openDb();
-    await db.exec('DROP TABLE IF EXISTS users');
-    await db.exec('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username Text NOT NULL, email Text NOT NULL, password Text NOT NULL);');
-    await db.exec('INSERT INTO users (username, email, password) VALUES ("Sabrina", "vonsab@gmx.at", "geheim47"),("Marcella", "marcella_merholz@web.de", "password7");');
-  }
-
-  async openDb() {
-    return sqlite.open({
-      filename: 'users.db',
-      driver: sqlite3.Database
-    })
   }
 
 }
